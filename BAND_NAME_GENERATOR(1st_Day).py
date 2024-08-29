5b9408e402d09249e1e0edc018ec9572f6ab6ff7
Ade20boss <danielademoye3@gmail.com>
2024-08-29 13:18:29 +0100

    Add files via upload

---
+++ BAND_NAME_GENERATOR(1st_Day).py
@@ -0,0 +1,4 @@
+print("WELCOME TO BAND NAME GENERATOR")
+city = input("What's the name of the city yu grew up in: \n")
+genre = input("What genre of music do you sing: \n")
+print("Your band name is " + city + " " + genre)
